21d9ae331208cf13723f55aeba889eb983566927
santhosh6194 <santhosh6194@gmail.com>
2024-12-17 11:03:21 +0530

    a55x: Build samsung doze

--- device.mk
+++ device.mk
@@ -209,6 +209,10 @@ PRODUCT_PACKAGES += \
     android.hardware.usb-service.samsung \
     android.hardware.usb.gadget-service.samsung
 
+# DOZE
+PRODUCT_PACKAGES += \
+    SamsungDoze
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
