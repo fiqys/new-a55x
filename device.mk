0d3bba6fd84dfabc13871a751af6aaa85c25ffc0
Majaahh <maja.blaszczak@proton.me>
2025-09-02 14:38:06 +0200

    a55x: Migrate to soong_config_set for libinit
    Change-Id: Ibe2fc2b23dfb32f804fbea87a8e61f634a064a8c
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- device.mk
+++ device.mk
@@ -212,6 +212,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+# Libinit
+$(call soong_config_set,libinit,vendor_init_lib,//$(COMMON_PATH)/libinit:libinit_s5e8845)
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.pixel \
