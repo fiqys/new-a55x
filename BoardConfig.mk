d192afd039d08d629b8500a0d3c0eafa3c68df04
0xCAFEBABE <me.cafebabe@gmail.com>
2025-09-02 11:55:34 +0200

    a55x: Include private sepolicy
    Change-Id: I32c274ca0995d0381c5589372358d46e0cfdbf72
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,7 +157,8 @@ include device/lineage/sepolicy/exynos/sepolicy.mk
 BOARD_SEPOLICY_TEE_FLAVOR := teegris
 include device/samsung_slsi/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 ## USB
 $(call soong_config_set,samsungUsbGadgetVars,gadget_name,13200000.dwc3)
