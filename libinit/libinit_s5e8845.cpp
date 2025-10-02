b11e858837a8c9d0e40ee982fd297540a57c5619
Majaahh <maja.blaszczak@proton.me>
2025-10-02 11:52:36 +0200

    a55x: Cleanup libinit
    Change-Id: I7cc43d0dab5aa144d5b7611ecf16bc4c33535af4
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- libinit/libinit_s5e8845.cpp
+++ libinit/libinit_s5e8845.cpp
@@ -4,9 +4,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <libinit_dalvik_heap.h>
+#include <libinit_utils.h>
 #include <libinit_variant.h>
-
 #include "vendor_init.h"
+
+#include <android-base/properties.h>
+
+using android::base::GetProperty;
 
 static const variant_info_t unknown = {
     .device = "unknown",
@@ -22,4 +27,13 @@ static const std::vector<variant_info_t> variants = {
 
 void vendor_load_properties() {
     search_variant(variants);
+
+    std::string model = GetProperty("ro.boot.product.model", "");
+    if (model.empty()) {
+        model = GetProperty("ro.boot.em.model", "");
+    }
+    set_ro_build_prop("model", model, true);
+    set_ro_build_prop("product", model, false);
+
+    set_dalvik_heap();
 }
