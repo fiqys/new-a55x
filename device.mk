955f236599f36a0edb3249b77923195913c78292
Mst8981 <unformed8981@gmail.com>
2025-10-03 17:20:42 +0900

    a55x: fingerprint: rename to s5e8845

--- device.mk
+++ device.mk
@@ -228,7 +228,7 @@ PRODUCT_PACKAGES += \
     android.hardware.light-service.samsung
 
 # Fingerprint
-PRODUCT_PACKAGES += android.hardware.biometrics.fingerprint-service.samsung
+PRODUCT_PACKAGES += android.hardware.biometrics.fingerprint-service.s5e8845
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
