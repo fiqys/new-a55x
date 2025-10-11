2314554eeb6af552ead18f40e9a8b70971eba89d
Mst8981 <unformed8981@gmail.com>
2025-10-12 07:58:09 +0900

    a55x: Update VENDOR_SECURITY_PATCH

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,7 +145,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 $(call soong_config_set,cbd,protocol,sipc)
 
 ## Security
-VENDOR_SECURITY_PATCH := 2025-07-01
+VENDOR_SECURITY_PATCH := 2025-09-01
 
 # Sepolicy
 include device/lineage/sepolicy/exynos/sepolicy.mk
