de0088209284bcbb43b463cd14c148cdad5a5acc
Mst8981 <unformed8981@gmail.com>
2025-10-14 07:49:58 +0900

    a55x: libinit: Add region suffix to variant names

--- libinit/libinit_s5e8845.cpp
+++ libinit/libinit_s5e8845.cpp
@@ -13,7 +13,7 @@
 
 using android::base::GetProperty;
 
-static const variant_info_t a55xnaxx = {
+static const variant_info_t a55xnaxx_eux = {
     .device = "a55x",
     .model = "SM-A556B",
     .name = "a55xnaxx",
@@ -21,7 +21,7 @@ static const variant_info_t a55xnaxx = {
     .build_desc = "a55xnaxx-user 14 UP1A.231005.007 A556BXXSABYI1 release-keys"
 };
 
-static const variant_info_t a55xnsxx = {
+static const variant_info_t a55xnsxx_xme = {
     .device = "a55x",
     .model = "SM-A556E",
     .name = "a55xnsxx",
@@ -30,8 +30,8 @@ static const variant_info_t a55xnsxx = {
 };
 
 static const std::vector<variant_info_t> variants = {
-    a55xnaxx,
-    a55xnsxx,
+    a55xnaxx_eux,
+    a55xnsxx_xme,
 };
 
 void vendor_load_properties() {
