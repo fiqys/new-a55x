d33359cd9aa84535ffddb401a71a69d14dc43da0
LuK1337 <priv.luk@gmail.com>
2025-10-15 20:09:18 +0530

    a55x: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643

--- device.mk
+++ device.mk
@@ -169,6 +169,9 @@ $(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
 
+# Overlays
+PRODUCT_PACKAGES += NcmTetheringOverlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
