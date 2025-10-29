398ce07646249a64aa9c8d4c79a896e85eb57938
ducthoe <mariofatrio@gmail.com>
2025-10-29 16:23:00 +0100

    a55x: libinit: Remove region differences via ro.boot.carrierid

--- configs/init/libinit/libinit_s5e8845.cpp
+++ configs/init/libinit/libinit_s5e8845.cpp
@@ -13,37 +13,25 @@
 
 using android::base::GetProperty;
 
-static const variant_info_t a55xnaxx_eux = {
+static const variant_info_t a55xnaxx = {
     .device = "a55x",
     .model = "SM-A556B",
-    .csc_code = "EUX",
     .name = "a55xnaxx",
     .build_fingerprint = "samsung/a55xnaxx/a55x:14/UP1A.231005.007/A556BXXSABYI1:user/release-keys",
     .build_desc = "a55xnaxx-user 14 UP1A.231005.007 A556BXXSABYI1 release-keys"
 };
 
-static const variant_info_t a55xnsxx_xme = {
+static const variant_info_t a55xnsxx = {
     .device = "a55x",
     .model = "SM-A556E",
-    .csc_code = "XME",
     .name = "a55xnsxx",
     .build_fingerprint = "samsung/a55xnsxx/a55x:14/UP1A.231005.007/A556EXXSABYI2:user/release-keys",
     .build_desc = "a55xnsxx-user 14 UP1A.231005.007 A556EXXSABYI2 release-keys"
 };
 
-static const variant_info_t a55xnsxx_xid = {
-    .device = "a55x",
-    .model = "SM-A556E",
-    .csc_code = "XID",
-    .name = "a55xnsxx",
-    .build_fingerprint = "samsung/a55xnsxx/a55x:14/UP1A.231005.007/A556EXXSABYI4:user/release-keys",
-    .build_desc = "a55xnsxx-user 14 UP1A.231005.007 A556EXXSABYI4 release-keys",
-};
-
 static const std::vector<variant_info_t> variants = {
-    a55xnaxx_eux,
-    a55xnsxx_xme,
-    a55xnsxx_xid,
+    a55xnaxx,
+    a55xnsxx,
 };
 
 void vendor_load_properties() {
