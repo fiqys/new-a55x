e01de9fc5605e60c8f5ac1fe46dff548289df01d
ducthoe <mariofatrio@gmail.com>
2025-11-02 20:25:10 +0100

    a55x: Stop building Boot Control HAL
    We are using the blobbed Exynos one anyway

--- device.mk
+++ device.mk
@@ -77,11 +77,6 @@ PRODUCT_COPY_FILES += \
     hardware/samsung_slsi/libbt/conf/bt_did.conf:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bt_did.conf \
     hardware/samsung_slsi/libbt/conf/bt_vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bt_vendor.conf
 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    com.android.hardware.boot \
-    android.hardware.boot-service.default_recovery
-
 # Characteristics
 PRODUCT_CHARACTERISTICS := phone
 
