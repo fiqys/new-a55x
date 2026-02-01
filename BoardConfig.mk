3eead2e3d90eb7d2ffc8a95f78398e0b9ddd10f4
luk1337 <priv.luk@gmail.com>
2026-02-01 22:08:53 +0300

    a55x: Remove vendor/lineage device framework matrix inclusion

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,7 +133,6 @@ include device/samsung_slsi/sepolicy/sepolicy.mk
 # Vintf
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
