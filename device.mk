86dd8efcc9362bded3ea77d3e7603f3f0b1d7d81
fiqxty <elsifycritic@gmail.com>
2026-02-02 17:13:24 +0300

    a55x: Update some soong config variables to bool type
    Migrate Lineage Health configuration flags to boolean types.

--- device.mk
+++ device.mk
@@ -154,9 +154,11 @@ PRODUCT_PACKAGES += vendor.lineage.health-service.default
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/batt_slate_mode)
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
-$(call soong_config_set,lineage_health,charging_control_supports_toggle,true)
-$(call soong_config_set,lineage_health,charging_control_supports_deadline,false)
+
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_toggle,true)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_deadline,false)
+
 $(call soong_config_set,lineage_health,fast_charge_node,/sys/class/sec/switch/afc_disable)
 $(call soong_config_set,lineage_health,fast_charge_value_none,1)
 $(call soong_config_set,lineage_health,fast_charge_value_fast_charge,0)
