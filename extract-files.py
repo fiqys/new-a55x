94b1ce63fca6809a8f7cc01f3a99d3ab258cc73a
fiqys <elsifycritic@gmail.com>
2026-06-24 10:32:30 +0300

    a55x: Stop replacing libprotobuf-cpp-full-21.7
    * Now bundled in lineage/compat

--- extract-files.py
+++ extract-files.py
@@ -102,8 +102,6 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/etc/init/vendor.samsung.hardware.camera.provider-service_64.rc': blob_fixup()
         .regex_replace('vendor_secdir w', 'w')
         .regex_replace('vendor_secdir', 'camera'),
-    'vendor/lib64/lib_profiler.so': blob_fixup()
-        .replace_needed('libprotobuf-cpp-full-21.7.so', 'libprotobuf-cpp-full-21.12.so'),
     (
         'vendor/lib64/libsensorlistener.so',
         'vendor/lib64/libvdis_core.so',
@@ -125,7 +123,6 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/etc/init/init.nfc.samsung.rc': blob_fixup()
         .regex_replace('system', 'secure_element'),
     'vendor/lib64/libsec-ril.so': blob_fixup()
-        .replace_needed('libprotobuf-cpp-full-21.7.so', 'libprotobuf-cpp-full-21.12.so')
         .sig_replace(
             '80 0e 40 f9 e1 03 16 aa 82 0c 80 52 e3 03 15 aa 24 00 80 52',
             '80 0e 40 f9 e1 03 16 aa 82 0c 80 52 03 00 80 d2 24 00 80 52'),
