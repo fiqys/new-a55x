a9f3558e98cbb8aeaee6a234f1ed8334ae08b1c2
Mst8981 <unformed8981@gmail.com>
2026-07-01 17:58:57 +0300

    a55x: Inherit dalvik heap config (xhdpi-6144)
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Enforce generic ramdisk allow list
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # Virtualization service
 $(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
 
