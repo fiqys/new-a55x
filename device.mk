372304ed89704ef2e8f40e43505e29dec1188f51
Siddeswar Aluganti <quic_salugant@quicinc.com>
2026-07-01 18:24:34 +0300

    a55x: Add hwservicemanager, allocator service to the device.
    Signed-off-by: fiqys <elsifycritic@gmail.com>
    Change-Id: Ib72af1dc3f950debdb2d1df75f53c7a9e6d56b65

--- device.mk
+++ device.mk
@@ -30,6 +30,12 @@ PRODUCT_SHIPPING_API_LEVEL := 34
 
 BOARD_BUILD_SUPER_IMAGE_BY_DEFAULT := true
 
+# HIDL
+# TODO(b/330696629) remove this once device can drop HIDL.
+PRODUCT_PACKAGES += \
+    hwservicemanager \
+    android.hidl.allocator@1.0-service
+
 # Graphics
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
