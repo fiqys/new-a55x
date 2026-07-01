d366de00ae620fe284d646b4b39b0ef37f1ae413
Tim Zimmerman <tim@linux4.de>
2026-07-01 12:36:35 +0300

    a55x: Switch to F2FS with fscrypt v2 for /data
    * Since users will be forced to wipe after the android-4.14-stable merge anyway, switch to a more future proof encryption setup
    
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- device.mk
+++ device.mk
@@ -6,13 +6,16 @@
 
 DEVICE_PATH := device/samsung/a55x
 
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(DEVICE_PATH)
-
 # Inherit from the proprietary version
 $(call inherit-product-if-exists, vendor/samsung/a55x/a55x-vendor.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Shipping level
 BOARD_SHIPPING_API_LEVEL := 34
 PRODUCT_SHIPPING_API_LEVEL := 34
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
