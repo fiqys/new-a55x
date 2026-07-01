ebbc56215add0b82833b56faaad53310ab828cbc
Mst8981 <unformed8981@gmail.com>
2026-07-01 17:56:19 +0300

    a55x: Declare support for Verified Boot
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- device.mk
+++ device.mk
@@ -194,6 +194,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
+# AVB2.0, to tell PackageManager that the system supports Verified Boot(PackageManager.FEATURE_VERIFIED_BOOT)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
+
 # Boot control HAL
 PRODUCT_PACKAGES += \
     com.android.hardware.boot \
