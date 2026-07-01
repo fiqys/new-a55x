0339ee574b55231a473cf2e0d0b05c20bdb3c275
Shashank Sivakumar <quic_shassiva@quicinc.com>
2026-07-01 15:20:51 +0300

    a55x: Enable Virtualization service
    Change-Id: Iaa373c0676219a724e2033d9a7863dfdd7b7504b
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Enforce generic ramdisk allow list
 $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 
+# Virtualization service
+$(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
