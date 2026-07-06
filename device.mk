df6a2e16f82b5396c0bb3f3054b256541d7e8c67
CmdCtrlDevic3 <cmdctrldevic3@proton.me>
2026-07-06 18:45:58 +0300

    a55x: Configure dim layer zorder
    Also move udfps layer to be above dim layer.
    
    Signed-off-by: fiqys <elsifycritic@gmail.com>
    Signed-off-by: CmdCtrlDevic3 <cmdctrldevic3@proton.me>

--- device.mk
+++ device.mk
@@ -107,7 +107,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += android.hardware.biometrics.fingerprint-service.samsung
 
 # UDFPS
-$(call soong_config_set,samsungUdfpsVars,udfps_zorder,0xff)
+$(call soong_config_set,samsungUdfpsVars,dim_layer_zorder,0xff)
+$(call soong_config_set,samsungUdfpsVars,udfps_zorder,0x100)
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/samsung/fingerprint:libudfps_extension.samsung)
 
 # Vibrator
